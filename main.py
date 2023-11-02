ddce7d3a040a56a5af515ace7c191c0fbf731bfe
Gabriel Mango <gabrielhmango@gmail.com>
2023-11-02 20:27:04 -0300

    refact: change getting manga title

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from pprint import pprint
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get('https://www.brmangas.net/manga/black-clover-online-1/')
 
 
-def wait_element(driver, x_path):
-    while True:
-        if driver.find_element(By.XPATH, x_path):
-            return driver.find_element(By.XPATH, x_path).text
-        time.sleep(1)
-
-
-title_xpath = '//*[@id="posttitle"]/div/h1'
-
-title = wait_element(driver, title_xpath)
+# Geting the title of the manga
 manga_title = ''
-for letter in title.split():
-    if letter not in ['LER', 'ONLINE']:
-        manga_title += letter.capitalize()
-        manga_title += ' '
+while True:
+    if driver.find_element(By.XPATH, '//*[@id="posttitle"]/div/h1'):
+        title = driver.find_element(By.XPATH, '//*[@id="posttitle"]/div/h1').text
 
+        for letter in title.split():
+            if letter not in ['LER', 'ONLINE']:
+                manga_title += letter.capitalize()
+                manga_title += ' '
+        break
+
+print(manga_title)
 
 driver.close()
