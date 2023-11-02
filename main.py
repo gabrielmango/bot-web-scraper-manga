69c8321f9003a16a36f98fd2d87a975d658a90d7
Gabriel Mango <gabrielhmango@gmail.com>
2023-11-02 20:03:12 -0300

    feat: implementing geting title of the manga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -7,6 +8,22 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 
 driver.get('https://www.brmangas.net/manga/black-clover-online-1/')
 
-time.sleep(5)
+
+def wait_element(driver, x_path):
+    while True:
+        if driver.find_element(By.XPATH, x_path):
+            return driver.find_element(By.XPATH, x_path).text
+        time.sleep(1)
+
+
+title_xpath = '//*[@id="posttitle"]/div/h1'
+
+title = wait_element(driver, title_xpath)
+manga_title = ''
+for letter in title.split():
+    if letter not in ['LER', 'ONLINE']:
+        manga_title += letter.capitalize()
+        manga_title += ' '
+
 
 driver.close()
