88e35d731bcdaa3d4f364dcda713afb1982882ce
Gabriel Mango <gabrielhmango@gmail.com>
2023-11-02 21:51:43 -0300

    refact: implamenting function wait_element

--- main.py
+++ main.py
@@ -21,34 +21,31 @@ def wait_element(value):
 
 # Getting manga title
 manga_title = ''
-while True:
-    if driver.find_element(By.XPATH, '//*[@id="posttitle"]/div/h1'):
-        title = driver.find_element(By.XPATH, '//*[@id="posttitle"]/div/h1').text
 
-        for letter in title.split():
-            if letter not in ['LER', 'ONLINE']:
-                manga_title += letter.capitalize()
-                manga_title += ' '
-        manga_title = manga_title.strip()
-        break
-    time.sleep(0.5)
+title = wait_element('#posttitle > div > h1').text
+
+for letter in title.split():
+    if letter not in ['LER', 'ONLINE']:
+        manga_title += letter.capitalize()
+        manga_title += ' '
+manga_title = manga_title.strip()
+
 
 # Getting manga chapters
 manga_chapters = {}
-chapters_xpath = '/html/body/div/div[1]/main/div/div[2]/div[2]/div[2]/div[1]/div[4]/ul'
-while True:
-    if driver.find_element(By.XPATH, chapters_xpath):
-        ul_element = driver.find_element(By.XPATH, chapters_xpath)
-        a_elements = ul_element.find_elements(By.TAG_NAME, 'a')
+chapters_css_selector = 'body > div > div.wrapper > main > div > div.row.interna > div.col-lg-10.col-md-9.col-sm-9.col-xs-12.col > div.manga > div.container_t > div.lista_manga > ul'
 
-        manga_chapters = {
-            element.text: element.get_attribute('href')
-            for element in a_elements
-        }
-        break
-    time.sleep(1)
-driver.close()
+ul_element = wait_element(chapters_css_selector)
+a_elements = ul_element.find_elements(By.TAG_NAME, 'a')
+
+manga_chapters = {
+    element.text: element.get_attribute('href')
+    for element in a_elements
+}
+
 
 # Create folder to store chapters
 if not os.path.exists(manga_title):
     os.makedirs(manga_title)
+
+driver.close()
